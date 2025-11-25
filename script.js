3a7acc4e3303a7884ba8fdff77c4bc059f3b3497
Sergiy <112825544+SerezhkaGit@users.noreply.github.com>
2025-11-25 22:33:17 +0200

    Update script.js-4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const birthdayDate = new Date("2025-11-25T22:15:00").getTime(); 
+const birthdayDate = new Date("2025-11-25T22:39:00").getTime(); 
 
 const countdownEl = document.getElementById('countdown-timer');
 const congratsButton = document.getElementById('congrats-button');
@@ -90,4 +90,5 @@ congratsButton.addEventListener('click', () => {
     setInterval(createConfettiPiece, 100);
 
 });
+
 
